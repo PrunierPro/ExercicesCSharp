76df128eb17fc09360ba7277e2793ad907a5e7a2
Thomas Prunier <thomas.prunier@labom2iformation.fr>
2023-07-13 16:45:47 +0200

    Exercices extras

---
+++ ExerciceADN/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+bool VerificationADN(string chaine)
+{
+    return !Regex.IsMatch(chaine, "[^actg]");
+}
+
+string SaisieADN(string message)
+{
+    Console.Write(message);
+    string chaine = Console.ReadLine().ToLower().Trim();
+    while (!VerificationADN(chaine))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("Saisie incorrecte ! Réessayez : ");
+        Console.ResetColor();
+        chaine = Console.ReadLine().ToLower().Trim();
+    }
+    return chaine;
+}
+
+double Proportion(string chaine, string sequence)
+{
+    int matchCount = Regex.Matches(chaine, sequence).Count;
+    return Math.Round(((double) matchCount * sequence.Length / chaine.Length) * 100, 2);
+}
+
+Console.WriteLine("--- ADN ---");
+string chaine = SaisieADN("Veuillez saisir une chaine adn : ");
+string sequence = SaisieADN("Veuillez saisir une sequence adn : ");
+Console.WriteLine($"La chaine {chaine} contient {Proportion(chaine, sequence)}% de {sequence}");
---
+++ ExerciceTuple/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("--- Opérations mathématiques ---");
+Console.Write("Entrez un premier nombre: ");
+double num1 = controleSaisieDouble();
+Console.Write("Entrez un second nombre: ");
+double num2 = controleSaisieDouble();
+
+(double somme, double diff, double quotient, double produit) resultat = Operation(num1, num2);
+Console.WriteLine($"{num1} + {num2} = {resultat.somme}\n{num1} - {num2} = {resultat.diff}\n{num1} / {num2} = {resultat.quotient}\n{num1} x {num2} = {resultat.produit}");
+
+
+(double somme, double diff, double quotient, double produit) Operation(double num1, double num2)
+{
+    return (num1+num2, num1-num2, num1/num2, num1*num2);
+}
+
+double controleSaisieDouble()
+{
+    double res;
+    while(!double.TryParse(Console.ReadLine(), out res))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("Vous devez entrer un nombre : ");
+        Console.ResetColor();
+    }
+    return res;
+}
